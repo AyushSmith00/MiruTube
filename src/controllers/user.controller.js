ee55354a1b17909a1ecd8f8418a151bdbc50adc3
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-08-30 18:16:59 +0530

    Eighth Commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
-
+import {User} from "../models/user.model.js"
 const registerUser = asyncHandler(async (req, res)=> {
 
 
@@ -12,6 +12,21 @@ const registerUser = asyncHandler(async (req, res)=> {
             throw new ApiError(400, "All fields are required")        
     }
 
+    const exixtedUser = User.findOne({
+        $or: [{email},{username}]
+    })
+
+    if(exixtedUser){
+        throw new ApiError(409, "User with same email or username already exixt")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalpath = req.files?.coverImage[0]?.path;
+
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Burh u need an Avator")
+    }
 })
 
 export {registerUser}
