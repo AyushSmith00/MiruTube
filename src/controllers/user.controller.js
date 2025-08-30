9c3efff965759f7c5299385b6d3de81e85701c12
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-08-30 14:48:38 +0530

    Updating userController

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler(async (req, res)=> {
-     res.status(200).json({
-        message: "ok"
-     })
+
+
+    const {fullname, email, username, password} = req.body
+    console.log("email", email);
+
+    if([fullname, email, password, username].some((field) => 
+        field?.trime() === "")){
+            throw new ApiError(400, "All fields are required")        
+    }
+
 })
 
 export {registerUser}
