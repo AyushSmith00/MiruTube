0fd09ed85eb53eeb4e44ee5465e41a12cf0f01e9
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-09-29 21:54:34 +0530

    created subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -182,4 +182,4 @@ const refreshAccessToken = asyncHandler(async(req, res) => {
 
 })
 
-export {registerUser, loginUser, logoutUser}
+export {registerUser, loginUser, logoutUser, refreshAccessToken}
